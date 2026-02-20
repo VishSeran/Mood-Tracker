1a2764b75174102bc552cf9fd59eebfb64597f7b
VishSeran <vishwa.sovis@gmail.com>
2026-02-20 18:35:31 +0530

    creatMood method in Main  class

--- src/MoodTracker.java
+++ src/MoodTracker.java
@@ -21,13 +21,13 @@ public class MoodTracker {
 
         while (running) {
 
-            System.out.println("\n press 1: Add a new Mood");
-            System.out.println("\n press 2: View all Moods");
-            System.out.println("\n press 3: Search Moods");
-            System.out.println("\n press 4: Edit Mood");
-            System.out.println("\n press 5: Delete Mood(s)");
-            System.out.println("\n press 6: Write Moods to a file");
-            System.out.println("\n press 7: Exit");
+            System.out.println("\npress 1: Add a new Mood");
+            System.out.println("press 2: View all Moods");
+            System.out.println("press 3: Search Moods");
+            System.out.println("press 4: Edit Mood");
+            System.out.println("press 5: Delete Mood(s)");
+            System.out.println("press 6: Write Moods to a file");
+            System.out.println("press 7: Exit\n");
 
             choice = Integer.parseInt(scanner.nextLine());
 
@@ -87,27 +87,27 @@ public class MoodTracker {
                 if (userDate.isEmpty() && userTime.isEmpty() && notes.isEmpty()) {
 
                     mood = new Mood(name);
-                    System.out.println("New mood created! " + mood);
+                    
 
                 } else if (userTime.isEmpty() && notes.isEmpty()) {
 
                     mood = new Mood(name, date);
-                    System.out.println("New mood created! " + mood);
+                    
 
                 } else if (notes.isEmpty()) {
 
                     mood = new Mood(name, date, time);
-                    System.out.println("New mood created! " + mood);
+                    
 
                 } else if (userDate.isEmpty() & userTime.isEmpty()) {
 
                     mood = new Mood(name, notes);
-                    System.out.println("New mood created! " + mood);
+                  
 
                 } else {
 
                     mood = new Mood(name, date, time, notes);
-                    System.out.println("New mood created! " + mood);
+                    
 
                 }
 
@@ -115,8 +115,9 @@ public class MoodTracker {
 
                 if (isValid) {
                     moodlist.add(mood);
-                    System.out.println("The mood has been added to the tracker");
-                    
+                    System.out.println("\nNew mood created!\n" + mood);
+                    System.out.println("The mood has been added to the tracker\n");
+
                 }
 
             } else {
@@ -125,6 +126,8 @@ public class MoodTracker {
 
         } catch (DateTimeException e) {
             System.out.println("Invalid date ot time!");
+        }catch( InvalidMoodException e){
+            System.out.println("The mood is not valid");
         }
 
     }
